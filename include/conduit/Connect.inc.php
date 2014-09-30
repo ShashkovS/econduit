a3034611fbe97739b895882008ac02bcb8d10513
Eugene Porshnev <eugene57@yandex.ru>
2014-09-30 18:54:13 +0400

    Первая попытка организовать бэкапирование

--- include/conduit/Connect.inc.php
+++ include/conduit/Connect.inc.php
@@ -1,14 +1,10 @@
 <?php
 
-if (!defined('IN_CONDUIT')){
+if (!defined('IN_CONDUIT') || !defined('IN_PHPBB')){
     // Попытка прямого доступа к файлу
     exit();
 }
 
-if (!defined('IN_PHPBB')){
-    // Попытка прямого доступа к файлу
-    exit();
-}
 
 $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '/home/host1333511/econduit.ru/htdocs/www/forum/';
 $phpEx = 'php';
@@ -19,20 +15,21 @@ $conduit_webroot = '/conduit';
 
 
 // Параметры SQL-соединения
+define('SQL', true);
 require_once('Credentials.inc.php');
 
 // Коннект к базе
 $conduit_db = new PDO(
-    "{$sql_credentials['driver']}:host={$sql_credentials['host']};dbname={$sql_credentials['db']}" ,
-    $sql_credentials['user'] ,
-    $sql_credentials['password'] ,
+    "{$credentials['SQL']['driver']}:host={$credentials['SQL']['host']};dbname={$credentials['SQL']['db']}" ,
+    $credentials['SQL']['login'] ,
+    $credentials['SQL']['password'] ,
     array(
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
     )
 );
 
 // Теперь логин и пароль можно забыть навсегда
-unset($sql_credentials);
+unset($credentials);
 
 // Часовой пояс
 date_default_timezone_set('Europe/Moscow');
