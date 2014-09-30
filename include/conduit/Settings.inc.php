3b86d2205fcd8d8462f4a90978b949bcd1801b3d
Eugene Porshnev <eugene57@yandex.ru>
2014-09-30 20:00:03 +0400

    Исправен баг в Settings

--- include/conduit/Settings.inc.php
+++ include/conduit/Settings.inc.php
@@ -6,11 +6,11 @@ if (!defined('IN_CONDUIT')){
 }
 
 $Settings = array (
-    'forum_absolute_path'   : '/home/host1333511/econduit.ru/htdocs/www/forum/' ,
-    'forum_absolute_link'   : 'http://econduit.ru/forum/' ,
-    'conduit_relative_link' : '/conduit/' ,
-    'timezone'              : 'Europe/Moscow' ,
-    'page_metrics'          : '' ,
+    'forum_absolute_path'   => '/home/host1333511/econduit.ru/htdocs/www/forum/' ,
+    'forum_absolute_link'   => 'http://econduit.ru/forum/' ,
+    'conduit_relative_link' => '/conduit/' ,
+    'timezone'              => 'Europe/Moscow' ,
+    'page_metrics'          => '' ,
 );
 
 ?>
