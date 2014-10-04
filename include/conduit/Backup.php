6d20faa05465363c71c5374a65191faf50662e21
Eugene Porshnev <eugene57@yandex.ru>
2014-10-04 22:08:39 +0400

    Переписал функцию бэкапа. Теперь работает выгрузка на Яндекс.Диск и нормально анализируются ошибки. Письмо от cron'а приходит только если ошибки были (и содержит полный лог).

--- include/conduit/Backup.php
+++ include/conduit/Backup.php
@@ -2,27 +2,62 @@
 
 define('IN_CONDUIT', true);
 
-define('SQL', true);
-define('WebDAV', true);
-require('Credentials.inc.php');
+function dump_db($filename, $credentials, &$work_log) {
+    $work_log .= "Starting database dump: {$credentials['db']}\n";
+    
+    $mysqldump_error = shell_exec("mysqldump --user={$credentials['login']} --password={$credentials['password']} {$credentials['db']} 2>tmp.err | gzip --best --force > \"$filename\" && cat tmp.err && rm tmp.err");
 
-
-$date = date('Y-m-d');
-// Файл на сервере, перед отсылкой
-$filename = "/home/host1333511/econduit.ru/backups/{$credentials['SQL']['db']}.$date.sql.gz";
-
-$output = `mysqldump --user={$credentials['SQL']['login']} --password={$credentials['SQL']['password']} {$credentials['SQL']['db']} | gzip -f > $filename`;
-if (!file_exists($filename)) {
-    echo "Error: Dump {$credentials['SQL']['db']} failed: $output\n";
-} else {
-    echo "Success: DB {$credentials['SQL']['db']} dumped\n";
-    echo "Files to upload: $filename\n";
-    echo `curl --user {$credentials['WebDAV']['login']}:{$credentials['WebDAV']['password']} -T $file {$credentials['WebDAV']['url']}`;   //если ругается на сертификат, можно добавить ключ -k
-    // Удаляем локальный файл
-    // unlink($filename);
+    // По идее есть версия без временного файла, но она почему-то падает с ошибкой парсинга (sh: -c: line 0: syntax error near unexpected token `>')
+    // $mysqldump_error = shell_exec("mysqldump --user={$credentials['login']} --password={$credentials['password']} {$credentials['db']} 2>&1 > >(gzip --best --force > $filename)");
+    
+    // Проверяем успешность дампа
+    if ($mysqldump_error === "") {
+        $work_log .= "DB successfully dumped.\n";
+        return true;
+    } else {
+        $work_log .= "Error:\n$mysqldump_error\n";
+        return false;
+    }
 }
-echo "\n";
 
-unset($credentials);
+function upload_file($filename, $credentials, &$work_log) {
+    $work_log .= "Starting file transfer: $filename ==> {$credentials['url']}\n";
+    $curl_output = shell_exec("curl --user {$credentials['login']}:{$credentials['password']} --upload-file \"$filename\" --url \"{$credentials['url']}\" --silent --verbose 2>&1");
+    // Анализируем ответ
+    preg_match_all('@HTTP/[0-9|.]+\s(\d{3})\s.*?\n@i', $curl_output, $matches);
+    foreach ($matches as $match) {
+        if ($match[1] === '201') {
+            $work_log .= "File successfully uploaded\n";
+            return true;
+        }
+    }
+    $work_log .= "Error:\n$curl_output\n";
+    return false;
+}
+
+function backup() {
+    define('SQL', true);
+    define('WebDAV', true);
+    require('Credentials.inc.php');
+    require('Settings.inc.php');
+
+    $date = date('Y-m-d');
+    // Файл на сервере, перед отсылкой
+    $filename = "{$Settings['tmp_backup_storage']}$date.{$credentials['SQL']['db']}.sql.gz";
+
+    $work_log = "$date. Starting backup process.\n";
+    if (
+        dump_db($filename, $credentials['SQL'], $work_log) and 
+        upload_file($filename, $credentials['WebDAV'], $work_log)
+    ) {         // Всё выполнено успешно
+        // Удаляем локальный файл
+        unlink($filename);
+    } else {    // Произошла какая-то ошибка. Выводим весь лог в stdout.
+        echo $work_log;
+    }
+}
+
+// Запускаем всё
+backup();
 
 ?>
