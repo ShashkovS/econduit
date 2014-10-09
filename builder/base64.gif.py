a54d57977d8371bc9c49065be2bd2b811623feff
Sergey Shashkov <sh57@yandex.ru>
2014-10-09 23:22:40 +0400

    Gif-ки для отображения процесса загрузки

---
+++ builder/base64.gif.py
@@ -0,0 +1,12 @@
+from base64 import *
+PATH = ''
+res = b64encode(open(PATH + '3px.gif', 'br').read())
+pre = b"background-image: url(data:image/gif;base64,"
+aft = b"); background-size: 25%;"
+f = open(PATH + '3px.css','bw')
+res = b64encode(open(PATH + '3px_error.gif', 'br').read())
+pre = b"background-image: url(data:image/gif;base64,"
+aft = b"); background-size: 25%;"
+f = open(PATH + '3px_error.css','bw')
+f.write(pre+res+aft)
+f.close()
